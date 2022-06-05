7169ffee4765326654c5abb37cd3894d7c85c359
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-06-05 19:50:12 +0100

    Custome contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import "./Contact.css";
+import phone from "./././../..//assets/icons/phone-solid.png";
+import email from "./././../..//assets/icons/envelope-solid.png";
+import facebook from "./././../..//assets/icons/facebook-brands.png";
+import instagram from "./././../..//assets/icons/instagram-brands.png";
+import whatsapp from "./././../..//assets/icons/whatsapp-brands.png";
 
 function Contact() {
     return (
@@ -12,11 +17,29 @@ function Contact() {
             <div className="Contact-container-middle">
                 <div className="Contact-container-middle-left">
                     <div className="Contact-container-middle-left-first-element">
+                        <img src={phone} alt="phone-icon" />
+                        <h2>+(509)440-256-51</h2>
                     </div>
                     <div className="Contact-container-middle-left-second-element">
+                        <img src={email} alt="email-icon" />
+                        <h2>mstanley.me@gmail.com</h2>
+                    </div>
+                    <div className="Contact-container-middle-left-third-element">
+                        <img src={facebook} className="filterColor" alt="facebook-icon" />
+                        <img src={instagram} className="filterColor" alt="instagram-icon" />
+                        <img src={whatsapp} className="filterColor" alt="whatsapp-icon" />
                     </div>
                 </div>
                 <div className="Contact-container-middle-right">
+                    <form className="contact-form" action="#" method="POST">
+                        <label htmlFor="name" className="labelName label">Name</label>
+                        <input type="text" name="name" className="inputName" placeholder="Your full name" required/>
+                        <label htmlFor="email" className="labelEmail label">Email</label>
+                        <input type="email" name="email" className="inputEmail" placeholder="Your email" required/>
+                        <label htmlFor="message" className="labelMessage label">Message</label>
+                        <textarea name="message" className="inputMessage" placeholder="Write your message..." required cols={30} rows={8}/>
+                        <input type="submit" value="Send" className="inputSubmit" />
+                    </form>
                 </div>
             </div>
         </div>
