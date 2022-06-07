d6c9541595dbeb721ff2b2801e276de51cf12148
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-06-07 13:33:32 +0100

    make change

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { useState } from "react";
 import img1 from "././../../assets/4AEF4B7D-AB3E-4860-A1C6-7BD6A9357407_1_105_c.jpeg";
-import img2 from "././../../assets/42D1FD7A-D780-4267-9F11-B6ACE87D1C36_1_105_c.jpeg";
-import img3 from "././../../assets/61AC484F-2F2C-4729-9773-EEE6EBBA22A1_1_105_c.jpeg";
-import img4 from "././../../assets/205D76DE-04DF-4FF0-9006-559DE96A36FB_1_105_c.jpeg";
-import img5 from "././../../assets/5145EDF7-DC48-4450-BC8B-24DD8E9B4D54_1_105_c.jpeg";
-import img6 from "././../../assets/B001F570-061D-4854-8731-5D67A785DCB9_1_105_c.jpeg";
-import img7 from "././../../assets/B9B12719-7F4E-4C10-B0FE-2BCEC7A71FDC_1_105_c.jpeg";
-import img8 from "././../../assets/F48EEC66-5258-4CAE-8CBF-FF046F5569D5_1_105_c.jpeg";
-import img9 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
-import img10 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
-import img11 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
-import img12 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
+import img2 from "././../../assets/F48EEC66-5258-4CAE-8CBF-FF046F5569D5_1_105_c.jpeg";
+import img3 from "././../../assets/42D1FD7A-D780-4267-9F11-B6ACE87D1C36_1_105_c.jpeg";
+// import img4 from "././../../assets/205D76DE-04DF-4FF0-9006-559DE96A36FB_1_105_c.jpeg";
+// import img5 from "././../../assets/5145EDF7-DC48-4450-BC8B-24DD8E9B4D54_1_105_c.jpeg";
+// import img6 from "././../../assets/B001F570-061D-4854-8731-5D67A785DCB9_1_105_c.jpeg";
+// import img7 from "././../../assets/B9B12719-7F4E-4C10-B0FE-2BCEC7A71FDC_1_105_c.jpeg";
+// import img8 from "././../../assets/61AC484F-2F2C-4729-9773-EEE6EBBA22A1_1_105_c.jpeg";
+// import img9 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
+// import img10 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
+// import img11 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
+// import img12 from "././../../assets/FA7DA059-707D-42B6-9810-67D7DDD871FA_1_105_c.jpeg";
 import "../Navigation/Navbar.js"
 import "./Work.css";
 
@@ -19,90 +19,39 @@ export default function Work() {
   const [work] = useState([
     {
       id: 1,
-      title: "Portrait Photography",
-      description: "In this section you'll see all available work I've been done in portrait photography. I've been done a lot of portraits for friends and family, and I've been also done some of my own.",
+      title: "Wedding Photography",
+      description: "In this section you'll see all available work I've been done in portrait photography.",
       image: `${img1}`,
-    },  
+    },
     {
       id: 2,
-      title: "Wedding Photography",
-      description: "In this section you'll see all available work I've been done in wedding photography. I've been done a lot of weddings for friends and family, and I've been also done some of my own.",
+      title: "Portrait Photography",
+      description: "In this section you'll see all available work I've been done in wedding photography.",
       image: `${img2}`,
     },
     {
       id: 3,
-      title: "Commercial Photography",
-      description: "In this section you'll see all available work I've been done in commercial photography. I've been done a lot of commercials for friends and family, and I've been also done some of my own.",
+      title: "Event Photography",
+      description: "In this section you'll see all available work I've been done in event photography.",
       image: `${img3}`,
-    },
-    {
-      id: 4,
-      title: "Nature Photography",
-      description: "In this section you'll see all available work I've been done in nature photography. I've been done a lot of nature for friends and family, and I've been also done some of my own.",
-      image: `${img4}`
-    },
-    {
-      id: 5,
-      title: "Street Photography",
-      description: "In this section you'll see all available work I've been done in street photography. I've been done a lot of street for friends and family, and I've been also done some of my own.",
-      image: `${img5}`,
-    },
-    {
-      id: 6,
-      title: "Architecture Photography",
-      description: "In this section you'll see all available work I've been done in architecture photography. I've been done a lot of architecture for friends and family, and I've been also done some of my own.",
-      image: `${img6}`,
-    },
-    {
-      id: 7,
-      title: "Nature Photography",
-      description: "In this section you'll see all available work I've been done in nature photography. I've been done a lot of nature for friends and family, and I've been also done some of my own.",
-      image: `${img7}`,
-    },
-    {
-      id: 8,
-      title: "Street Photography",
-      description: "In this section you'll see all available work I've been done in street photography. I've been done a lot of street for friends and family, and I've been also done some of my own.",
-      image: `${img8}`,
-    },
-    {
-      id: 9,
-      title: "Architecture Photography",
-      description: "In this section you'll see all available work I've been done in architecture photography. I've been done a lot of architecture for friends and family, and I've been also done some of my own.",
-      image: `${img9}`,
-    },
-    {
-      id: 10,
-      title: "Nature Photography",
-      description: "In this section you'll see all available work I've been done in nature photography. I've been done a lot of nature for friends and family, and I've been also done some of my own.",
-      image: `${img10}`
-    },
-    {
-      id: 11,
-      title: "Street Photography",
-      description: "In this section you'll see all available work I've been done in street photography. I've been done a lot of street for friends and family, and I've been also done some of my own.",
-      image: `${img11}`,
-    },
-    {
-      id: 12,
-      title: "Architecture Photography",
-      description: "In this section you'll see all available work I've been done in architecture photography. I've been done a lot of architecture for friends and family, and I've been also done some of my own.",
-      image: `${img12}`
     },
   ]);
   return (
     <div id="Work">
-      <div className="work-container">
+        <div className="explore">
+          <h3>Explore more</h3>
+          <span className="exploreBar"></span>
+        </div>
+        <div className="work-container">
         {work.map((work) => (
           <div className="work-card" key={work.id}>
-              <img className="img-card" src={work.image} alt={work.title} />
+            <img className="img-card" src={work.image} alt={work.title} />
             <div className="work-card-text">
               <h3>{work.title}</h3>
               <p>{work.description}</p>
-              <a href={work.link} target="_blank" rel="noopener noreferrer">
-                <button>View</button>
+              <a className="previewBtn" href={work.link} target="_blank" rel="noopener noreferrer">
+                <button>Show All</button>
               </a>
-                <button>Comment</button>
             </div>
           </div>
         ))}
